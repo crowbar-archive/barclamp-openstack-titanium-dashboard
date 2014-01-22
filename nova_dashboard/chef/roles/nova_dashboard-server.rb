081a357a7e51a41f30a4b9ad88debe413eba48c4
Sak <sakvinder.rai@gmail.com>
2014-01-22 10:10:25 +0000

    fixed error, added a comma before the new recipe

--- nova_dashboard/chef/roles/nova_dashboard-server.rb
+++ nova_dashboard/chef/roles/nova_dashboard-server.rb
@@ -2,7 +2,7 @@ name "nova_dashboard-server"
 description "Nova Dashboard Server Role"
 run_list(
          "recipe[nova_dashboard::server]",
-         "recipe[nova_dashboard::monitor]"
+         "recipe[nova_dashboard::monitor]",
          "recipe[nova_dashboard::boot_from_volume]"
 )
 default_attributes()
