6a35e79ef270c4e22ab3df231e4b25a25753184a
Sak <sakvinder.rai@gmail.com>
2014-01-22 08:56:07 +0000

    boot from volume functionlity added to horizon

--- nova_dashboard/chef/roles/nova_dashboard-server.rb
+++ nova_dashboard/chef/roles/nova_dashboard-server.rb
@@ -3,6 +3,7 @@ description "Nova Dashboard Server Role"
 run_list(
          "recipe[nova_dashboard::server]",
          "recipe[nova_dashboard::monitor]"
+         "recipe[nova_dashboard::boot_from_volume]"
 )
 default_attributes()
 override_attributes()
